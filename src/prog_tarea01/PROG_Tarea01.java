5ad9f3058ffa7396435e94c849feedaf4627016a
JaviBBivaJ <mail@mail.com>
2018-10-12 17:39:26 +0200

    commit1

---
+++ src/prog_tarea01/PROG_Tarea01.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog_tarea01;
+
+/**
+ *
+ * @author JaviB BivaJ
+ */
+public class PROG_Tarea01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            int i;            
+            System.out.println("Módulo Profesional - PROGRAMACIÓN\nUNIDAD DE TRABAJO 01"); 
+            System.out.println("Introducción a la programación\nJavier Blanco");
+            System.out.println("\nPero como soy muy pesado insisto dos veces más.");
+            for (i=1; i<3; i++)
+            {
+                System.out.println("Insisto. Vez: " + i);
+                System.out.println("Módulo Profesional - PROGRAMACIÓN\nUNIDAD DE TRABAJO 01"); 
+            }
+    }
+    
+}
